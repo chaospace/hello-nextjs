725cf6b72522a2ce80780973e8596e0edf610738
chaospace <ichaospace@gmail.com>
2023-02-08 12:11:34 +0900

    [UI/셀렉트박스] 셀렉트 박스 검색 기능 추가 기존 click을 focus로 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Select />
+      <Select defaultValue="농구" />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
