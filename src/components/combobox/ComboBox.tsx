3c0c0de76fe474b9bd51994fc054d26b72a719b7
chaospace <ichaospace@gmail.com>
2023-02-12 12:17:22 +0900

    [UI/Combobox] 콤보박스 동작 보완( 포커스, 필터, 컨트롤 버튼 이벤트 처리 )

--- src/components/combobox/ComboBox.tsx
+++ src/components/combobox/ComboBox.tsx
@@ -4,9 +4,11 @@ import {
   ChangeEvent,
   PropsWithChildren,
   useCallback,
+  useEffect,
   useRef,
   useState
 } from "react";
+import { addElementOutSideMouseEvent } from "../funcs";
 import styles from "./combobox.module.scss";
 
 interface ComboBoxOptions {
@@ -17,10 +19,18 @@ interface ComboBoxOptions {
 }
 
 function ComboBox({
-  children,
-  options = ["중국", "닌텐도", "플레이스테이션", "엑스박스", "게임보이"]
+  options = [
+    "중국",
+    "닌텐도",
+    "플레이스테이션",
+    "엑스박스",
+    "게임보이",
+    "재믹스",
+    "패밀리",
+    "애플코리아"
+  ]
 }: PropsWithChildren<{ options: ComboBoxOptions[] | string[] }>) {
-  const inputRef = useRef<HTMLInputElement>(null as any);
+  const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
   const [provider, setProvider] = useState<ComboBoxOptions[]>(
     options.map((option, id) => {
@@ -29,44 +39,79 @@ function ComboBox({
         : option;
     })
   );
+
   const selected = useRef<ComboBoxOptions[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null as any);
+  const comboboxRef = useRef<HTMLDivElement>(null as any);
+  const placeholder = "선택해주세요...";
+  const hasSelect = selected.current.length > 0;
+
+  // 검색어 반영 목록
+  const filteredProvider = provider.filter(o => o.label.indexOf(search) > -1);
+
+  /**
+   * 콤보박스 options정보 제어 함수
+   * selected 목록에 있는 요소를 제외한 select값을 초기화 처리
+   */
+  const updateProvider = useCallback(() => {
+    setProvider(prev =>
+      prev.map(o => ({
+        ...o,
+        select: selected.current.some(vo => vo.id === o.id)
+      }))
+    );
+  }, []);
 
   const onPointerDownCombobox = useCallback((isOpen: boolean) => {
-    if (isOpen) {
-      setOpen(false);
-    } else {
+    const hasFocus = document.activeElement === inputRef.current;
+    if (!isOpen) {
       setOpen(true);
+    }
+    if (!hasFocus) {
       inputRef.current.focus();
     }
   }, []);
 
-  const onDeleteSelect = useCallback((option: ComboBoxOptions) => {
-    selected.current = selected.current.filter(o => o.id !== option.id);
-    setProvider(prev =>
-      prev.map(o => ({
-        ...o,
-        select: selected.current.some(vo => vo.id === o.id)
-      }))
-    );
-  }, []);
-
-  const onClickOptionItem = useCallback((option: ComboBoxOptions) => {
-    if (option.select) {
+  const onDeleteSelect = useCallback(
+    (option: ComboBoxOptions) => {
       selected.current = selected.current.filter(o => o.id !== option.id);
-    } else {
-      selected.current.push({ ...option, select: true });
-    }
+      updateProvider();
+    },
+    [updateProvider]
+  );
 
-    setProvider(prev =>
-      prev.map(o => ({
-        ...o,
-        select: selected.current.some(vo => vo.id === o.id)
-      }))
+  // 선택목록 토글처리
+  // 전체 options목록 정보 갱신을 위해 updateProvider호출
+  const onClickOptionItem = useCallback(
+    (option: ComboBoxOptions) => {
+      if (option.select) {
+        selected.current = selected.current.filter(o => o.id !== option.id);
+      } else {
+        selected.current.push({ ...option, select: true });
+      }
+      updateProvider();
+    },
+    [updateProvider]
+  );
+
+  // 스테이지 클릭 시 이벤트 처리
+  useEffect(() => {
+    const onClickDocument = (event: MouseEvent, childNodeEvent: boolean) => {
+      if (!childNodeEvent && open) {
+        setOpen(false);
+      }
+    };
+
+    const removeElementOutSideMouseEvent = addElementOutSideMouseEvent(
+      comboboxRef.current,
+      onClickDocument
     );
-  }, []);
+
+    return removeElementOutSideMouseEvent;
+  }, [open]);
 
   return (
-    <div className={styles.combobox}>
+    <div className={styles.combobox} ref={comboboxRef}>
       <div className={styles.combobox__inputContainer}>
         <div
           className={styles.combobox__inputControls}
@@ -91,41 +136,69 @@ function ComboBox({
                 </span>
               );
             })}
+            <label className={styles.combobox__autoSizeInputWrapper}>
+              <input
+                ref={inputRef}
+                className={styles.combobox__autoSizeInput}
+                type="text"
+                placeholder={hasSelect ? "" : placeholder}
+                size={hasSelect ? 1 : placeholder.length * 2}
+                value={search}
+                onInput={(event: ChangeEvent<HTMLInputElement>) => {
+                  const inputValue = event.target.value;
+                  (event.target.parentNode as HTMLElement).dataset.value =
+                    inputValue;
+                  setSearch(inputValue);
+                }}
+              />
+            </label>
           </div>
-          <label className={styles.combobox__autoSizeInputWrapper}>
-            <input
-              ref={inputRef}
-              className={styles.combobox__autoSizeInput}
-              type="text"
-              size={1}
-              onInput={(event: ChangeEvent<HTMLInputElement>) => {
-                (event.target.parentNode as HTMLElement).dataset.value =
-                  event.target.value;
-              }}
-            />
-          </label>
         </div>
         <div className={styles.combobox__buttonControls}>
-          <button className={styles.combobox__button}>x</button>
+          <button
+            className={styles.combobox__button}
+            onClick={() => {
+              setSearch("");
+            }}
+          >
+            x
+          </button>
           <span className={styles.combobox__seperator}></span>
-          <button className={styles.combobox__button}>▾</button>
+          <button
+            className={styles.combobox__button}
+            onClick={() => setOpen(!open)}
+          >
+            {open ? "▴" : "▾"}
+          </button>
         </div>
       </div>
       {open && (
         <ul className={styles.combobox__optionContainer}>
-          {provider.map(option => {
-            return (
-              <li
-                key={option.value}
-                className={`${styles.combobox__optionItem} ${
-                  option.select ? styles.combobox__optionItemSelect : ""
-                }`}
-                onClick={() => onClickOptionItem(option)}
-              >
-                {option.label}
-              </li>
-            );
-          })}
+          {filteredProvider.length ? (
+            filteredProvider.map(option => {
+              return (
+                <li
+                  key={option.value}
+                  className={`${styles.combobox__optionItem} ${
+                    option.select ? styles.combobox__optionItemSelect : ""
+                  }`}
+                  onClick={() => onClickOptionItem(option)}
+                >
+                  {option.label}
+                </li>
+              );
+            })
+          ) : (
+            <li
+              className={styles.combobox__optionItem}
+              onClick={() => {
+                setOpen(!open);
+                setSearch("");
+              }}
+            >
+              검색결과가 없습니다.
+            </li>
+          )}
         </ul>
       )}
     </div>
