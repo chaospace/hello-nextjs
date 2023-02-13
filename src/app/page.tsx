7c570c8a23c3072c14f2c01907ab728d0ad03d08
chaospace <ichaospace@gmail.com>
2023-02-13 10:29:14 +0900

    [리팩토링] eslint 경고 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import ComboBox from "@/components/combobox/ComboBox";
+import MatchingGameApp from "@/components/matchingGame/MatchingGameApp";
 import Select from "@/components/Select";
 import styles from "./page.module.css";
 
-//const inter = Inter({ subsets: ["latin"] });
+//const _inter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
+  // const { matchingList } = getGameData();
+  // console.log("matchingList", matchingList);
   return (
     <main className={styles.main}>
       <Select multiple />
 
       <ComboBox />
+
+      <MatchingGameApp />
     </main>
   );
 }
