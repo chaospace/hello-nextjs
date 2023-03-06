b2f350ff0237b6ecf8c2bdbef25e5e47d2f0441d
chaospace <ichaospace@gmail.com>
2023-03-06 09:59:19 +0900

    [설정] jest설정 및 테스팅 모듈 추가

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 // jest config
 const nextJest = require("next/jest");
-
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./"
@@ -52,8 +51,12 @@ const customJestConfig = {
   moduleNameMapper: {
     "@/(.*)$": "<rootDir>/src/$1"
   },
-  testEnvironment: "jest-environment-jsdom"
-  // transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`]
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }]
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
